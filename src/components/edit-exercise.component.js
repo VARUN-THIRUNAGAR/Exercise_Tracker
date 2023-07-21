ad5a6ba209dc99bf7ac14600b930f563c8b8ce0e
VARUN-THIRUNAGAR <108337796+VARUN-THIRUNAGAR@users.noreply.github.com>
2023-07-21 18:44:49 +0530

    Add files via upload

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,108 @@
+// EditExercises.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { useParams, useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const EditExercises = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [username, setUsername] = useState('');
+  const [description, setDescription] = useState('');
+  const [duration, setDuration] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/exercises/' + id)
+      .then(response => {
+        setUsername(response.data.username);
+        setDescription(response.data.description);
+        setDuration(response.data.duration);
+        setDate(new Date(response.data.date));
+      })
+      .catch(err => console.log(err));
+
+    axios.get('http://localhost:3000/users/')
+      .then(response => {
+        if (response.data.length > 0) {
+          setUsers(response.data.map(user => user.username));
+        }
+      })
+      .catch(err => console.log(err));
+  }, [id]);
+
+  const onChangeUserName = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const onChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const onChangeDuration = (e) => {
+    setDuration(e.target.value);
+  };
+
+  const onChangeDate = (date) => {
+    setDate(date);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const exercise = {
+      username,
+      description,
+      duration,
+      date
+    };
+
+    axios.post('http://localhost:3000/exercises/update/' + id, exercise)
+      .then(res => {
+        console.log(res.data);
+        toast.success('Exercise updated successfully!');
+        navigate('/'); // Redirect to home page using navigate
+      })
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <div>
+      <h3>Edit Exercise Log</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select required className="form-control" value={username} onChange={onChangeUserName}>
+            {users.map(user => (
+              <option key={user} value={user}>{user}</option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input type="text" required className="form-control" value={description} onChange={onChangeDescription} />
+        </div>
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input type="text" required className="form-control" value={duration} onChange={onChangeDuration} />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker selected={date} onChange={onChangeDate} />
+          </div>
+        </div>
+        <div className="form-group">
+          <input type="submit" value="Update Exercise" className="btn btn-primary" />
+        </div>
+      </form>
+      <ToastContainer position="bottom-right" />
+    </div>
+  );
+};
+
+export default EditExercises;
